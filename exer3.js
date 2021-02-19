8aebd2c4c66aab961586edf2d2e19edf0572b247
Jeanmarie Quijada <jeanmarie.q@gmail.com>
2021-02-18 23:55:38 -0300

    solicitando dados

---
+++ exer3.js
@@ -0,0 +1,12 @@
+let nome = prompt("Qual é o seu nome");
+let sobrenome = prompt("Qual é o seu sobrenome");
+let idade = prompt("Qual é sua idade");
+
+console.log ("Bem-vindo", nome, "Adoramos tê-lo aqui, abaixo os dados que forneço para nós");
+console.log("");
+console.log('Dados do usuario')
+console.log (nome);
+console.log (sobrenome);
+console.log (idade);
+
+
